bc72d3767e130a712ed4ac1d2afa4b979a39adc1
Simran <simran.abbasov.00@mail.ru>
2023-03-08 09:55:54 +0400

    first commit

---
+++ QUIZ/app.js
@@ -0,0 +1,82 @@
+function Question(text,cohices,answer) {
+    this.text=text;
+    this.cohices=cohices;
+    this.answer=answer;
+}
+
+Question.prototype.checkanswer=function (answer) {
+    return this.answer===answer;
+}
+// Quiz construsction
+function Quiz(question) {
+    this.question=question;
+    this.score=0;
+    this.questionIndex=0;
+    
+}
+
+Quiz.prototype.getQuestion=function () {
+    return this.question[this.questionIndex]
+}
+
+Quiz.prototype.isfinish=function () {
+    return this.question.length===this.questionIndex;
+}
+
+Quiz.prototype.guess=function(answer){
+    var question=this.getQuestion();
+    if (question.checkanswer(answer)) {
+        this.score++;
+    }this.questionIndex++;
+}
+
+var q1=new Question("What's the best programng language?",['java','c#','javascript','python'],'javascript');
+var q2=new Question("What isn't  programng language?",['c++','c','word','javascript'],'javascript');
+var q3=new Question("What's the new  programng language?",['javascript','c#','css','sql'],'javascript');
+
+console.log(q1.checkanswer('javascript'));
+
+
+var question=[q1,q2,q3]
+
+var quiz=new Quiz(question);
+
+
+loadQuestion();
+
+function loadQuestion() {
+    if (quiz.isfinish()) {
+        showscore();
+    }
+    else{
+        var question=quiz.getQuestion();
+        var cohices=question.cohices;
+        document.querySelector('#question').textContent=question.text; 
+
+        for (let i = 0; i < cohices.length; i++) {
+            var element = document.querySelector('#choice'+i);
+            element.innerHTML= cohices[i];
+
+            guess('btn'+i,cohices[i]);            
+        }
+        showprogress();
+    }
+}
+
+function guess(id,guess) {
+    var btn=document.getElementById(id);
+    btn.onclick=function () {
+        quiz.guess(guess);
+        loadQuestion();
+        
+    }
+}
+function showscore() {
+    var html=`<h2>Score</h2><h4>${quiz.score}</h4>`;
+    document.querySelector(`.card-body`).innerHTML=html;
+}
+function showprogress() {
+    var totalquestion=quiz.question.length;
+    var questionNumber=quiz.questionIndex+1;
+    document.querySelector('#progress').innerHTML=`Question: ${totalquestion}  of: ${questionNumber}`;
+}
